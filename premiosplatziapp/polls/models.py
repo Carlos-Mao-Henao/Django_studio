c1827914704442bf6cc0e635ce7e5a1432dc9c0e
mao_henao_94 <carlos.henaoj94@gmail.com>
2022-05-24 23:43:27 -0500

    ingreso de la aplicación polls a settigns

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,3 +1,14 @@
+from ast import mod
+from secrets import choice
+from tkinter import CASCADE
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    #every time when we find a class who finnisg with "field" It's a datatype that we can use for a attribute 
+    pub_date = models.DateTimeField("date published")
+
+class Choices(models.Model):
+        question = models.ForeignKey(Question, on_delete=CASCADE)
+        choice_text = models.CharField(max_length=200)
+        votes = models.IntegerField(default=0)
