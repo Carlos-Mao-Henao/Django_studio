69254d7b1c897428c5ca283beecdd02ca336bab6
mao_henao_94 <carlos.henaoj94@gmail.com>
2022-05-30 23:26:44 -0500

    primera versión del html de los premios

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,3 +1,6 @@
+from multiprocessing.spawn import import_main_path
 from django.contrib import admin
 from .models import Question
+from .models import Choice
 admin.site.register(Question)
+admin.site.register(Choice)
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 # Create a Http reponse
 from django.http import HttpResponse
-# Create your views here.
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello world")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
 #We create de Http response in this funtion
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta número {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
+
