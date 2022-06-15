88b1751b1b464c3913f2ad44a50bf055af3e3ae1
mao_henao_94 <carlos.henaoj94@gmail.com>
2022-06-14 23:28:11 -0500

    final de los test

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -39,6 +39,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name="polls/detail.html"
+    
+    def get_queryset(self):
+        """
+        exclude any questions that aren't published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultView(generic.DetailView):
     model = Question
